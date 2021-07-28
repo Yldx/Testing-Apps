075d3c74867aaa5505f57ee48acb4024fa7eb878
Lioncake <37010597+Yldx@users.noreply.github.com>
2021-07-28 16:59:09 -0500

    Add files via upload

---
+++ image_to_chracters/zi.py
@@ -0,0 +1,50 @@
+# -- coding=utf-8 --
+from PIL import Image
+import argparse
+
+#命令行输入参数处理
+parser = argparse.ArgumentParser()
+parser.add_argument('--file')     #输入文件
+parser.add_argument('-o', '--output')   #输出文件
+parser.add_argument('--width', type = int, default = 80) #输出字符画宽
+parser.add_argument('--height', type = int, default = 80) #输出字符画高
+
+#超参数设定
+args = parser.parse_args()
+IMG = "C:\\baobro.jpg"
+WIDTH = 240
+HEIGHT = 30
+OUTPUT = "E:\\1.txt"
+#字符序列
+ascii_char = list("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
+# 将256灰度映射到70个字符上
+def get_char(r,g,b,alpha = 256):
+    if alpha == 0:
+        return ' '
+    length = len(ascii_char)
+    gray = int(0.2126 * r + 0.7152 * g + 0.0722 * b)
+
+    unit = (256.0 + 1)/length
+    return ascii_char[int(gray/unit)]
+
+if __name__ == '__main__':
+	#读取图片
+    im = Image.open(IMG)
+    #resize
+    im = im.resize((WIDTH,HEIGHT), Image.NEAREST)
+
+    txt = ""
+
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            txt += get_char(*im.getpixel((j,i)))
+        txt += '\n'
+	#打印
+    print(txt)
+    #字符画输出到文件
+    if OUTPUT:
+        with open(OUTPUT,'w') as f:
+            f.write(txt)
+    else:
+        with open("output.txt",'w') as f:
+            f.write(txt)
